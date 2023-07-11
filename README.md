cae432e2f0e69b4ddb9f6934dbda8aa88c660fc5
joycewachira <136504713+joycewachira@users.noreply.github.com>
2023-07-11 09:18:11 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,9 +1,63 @@
-## How to run the assignment
+# week-three-code-challenge
+ ## Movie Ticket App
+This is a JavaScript application that allows users to buy tickets for movies. The app fetches data from an API and dynamically manipulates the DOM to display the movie details and a button to buy tickets.
 
-<p>Install http-server globally by running the following command in the terminal:</p>
+## Description
+The Movie Ticket App fetches data from the following API endpoint:
 
-`npm install -g http-server`
+GET /films
 
-<p>Open a terminal, navigate to your project directory, and start the HTTP server by running the following command:</p>
+The app then dynamically creates HTML elements to display the movie details, including the title, runtime, showtime, and available tickets. Users can click the "Buy Ticket" button to decrease the number of available tickets by 1. If there are no available tickets, the button will be disabled.
 
-`http-server`
+## usage
+1.Clone the repository:
+`https://github.com/joycewachira/code-challenge-week-3-`
+2.Navigate to project folder
+`cd week-three-code-challenge`
+3.Install the dependencies:
+    `npm install`
+4.Run the app:
+`npm start`
+*Install the json-server using the command `npm insall -g json-server`
+*to get backend started run this command `json-server\\watch db.json`
+*Test your server by visiting this route in the browser `http://localhost:3000/films`
+
+## Core Deliverables
+
+As a user, I can:
+
+1. See the first movie's details, including its **poster, title, runtime,
+   showtime, and available tickets** when the page loads.
+2. See a menu of all movies on the left side of the page in the `ul#films`
+   element when the page loads.
+3. Buy a ticket for a movie. After clicking the "Buy Ticket" button, I should
+   see the number of available tickets decreasing on the frontend. I should not
+   be able to buy a ticket if the showing is sold out (if there are 0 tickets
+   available).
+
+## Bonus Deliverables
+
+1. Click on a movie in the menu to replace the currently displayed movie's
+   details with the new movie's details.
+2. When a movie is sold out (when there are no available tickets remaining),
+   indicate that the movie is sold out by changing the button text to "Sold
+   Out". Also update the film item in the `ul#films` menu by adding a class of
+   `sold-out` to the film.
+
+## Extra Bonus
+
+1. When a ticket is purchased, persist the updated number of `tickets_sold` on
+   the server.
+2. Delete a film from the server.
+
+## Technologies Used
+JavaScript
+HTML
+CSs
+
+
+## Author
+Author:[Joyce Wachira]
+
+ ## License
+ MIT License
--- index.js
+++ index.js
@@ -1,53 +1,67 @@
-// Global variables
-const filmList = document.getElementById("films");
-const filmTitle = document.getElementById("film-title");
-const filmRuntime = document.getElementById("film-runtime");
-const filmShowtime = document.getElementById("film-showtime");
-const filmAvailableTickets = document.getElementById("film-available-tickets");
-const filmPoster = document.querySelector(".film-poster img");
+const menu = document.getElementById("menu");
+const filmList = document.getElementById("filmlist");
 
-// Fetch film data
-const xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === XMLHttpRequest.DONE) {
-    if (xhr.status === 200) {
-      const data = JSON.parse(xhr.responseText);
-      const films = data.films;
-      // Populate film list
-      films.forEach((film) => {
-        const li = document.createElement("li");
-        const span = document.createElement("span");
-        span.classList.add("film-title");
-        span.textContent = film.title;
-        li.appendChild(span);
-        filmList.appendChild(li);
-      });
-      // Display first film's details
-      displayFilmDetails(films[0]);
-      // Add click event listener to film titles
-      const filmTitles = document.querySelectorAll(".film-title");
-      filmTitles.forEach((filmTitle) => {
-        filmTitle.addEventListener("click", () => {
-          const selectedFilm = films.find(
-            (film) => film.title === filmTitle.textContent
-          );
-          displayFilmDetails(selectedFilm);
-        });
-      });
-    } else {
-      console.error("Error: " + xhr.status);
-    }
+menu.addEventListener("click", () => {
+  if (filmList.style.display !== "none") {
+    filmList.style.display = "none";
+  } else {
+    filmList.style.display = "block";
   }
-};
-xhr.open("GET", "db.json", true);
-xhr.send();
+});
 
-// Display film details
-function displayFilmDetails(film) {
-  filmTitle.textContent = film.title;
-  filmRuntime.textContent = film.runtime;
-  filmShowtime.textContent = film.showtime;
-  const availableTickets = film.capacity - film["tickets_sold"];
-  filmAvailableTickets.textContent = availableTickets.toString();
-  filmPoster.src = film.poster;
-}
+const fetchFilms = () => {
+  fetch("http://localhost:3000/films")
+    .then(res => res.json())
+    .then(films => {
+      films.forEach(film => displayFilmList(film));
+    });
+};
+
+const displayFilmList = film => {
+  const listItem = document.createElement("li");
+  listItem.textContent = film.title;
+  filmList.appendChild(listItem);
+
+  listItem.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const name = document.querySelector("#headers h3");
+    name.innerHTML = film.title;
+
+    const runtime = document.getElementById("runtime");
+    runtime.textContent = `Runtime: ${film.runtime}`;
+
+    const showtime = document.getElementById("showtime");
+    showtime.textContent = `Showtime: ${film.showtime}`;
+
+    const availableTickets = document.getElementById("availabletickets");
+    const result = film.capacity - film.tickets_sold;
+    availableTickets.textContent = `Available Tickets: ${result}`;
+
+    const image = document.querySelector("#section img");
+    image.src = film.poster;
+
+    const description = document.querySelector("p");
+    description.textContent = `Description: ${film.description}`;
+
+    const button = document.getElementById("button");
+    button.textContent = "BUY TICKET";
+    button.style.backgroundColor = "green";
+    button.style.width = "150px";
+    button.style.height = "50px";
+    button.style.fontSize = "20px";
+
+    button.addEventListener("click", () => {
+      result--;
+      availableTickets.textContent = `Available Tickets: ${result}`;
+
+      if (result <= 0) {
+        button.textContent = "Sold out";
+        availableTickets.textContent = "Available Tickets: Sold out";
+        button.style.backgroundColor = "red";
+      }
+    });
+  });
+};
+
+fetchFilms();
